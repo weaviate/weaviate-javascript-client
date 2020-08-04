8253a21ecb0c9d6bef25ebad199acf337dc39233
Etienne Dilocker <etienne@dilocker.de>
2020-08-04 10:33:48 +0200

    gh-1 extend e2e test

--- end2end.test.js
+++ end2end.test.js
@@ -5,9 +5,24 @@ describe('an end2end test against a deployed instance', () => {
     host: 'demo.dataset.playground.semi.technology',
   });
 
-  test('the typed graphql get method', () => {
+  test('the typed graphql get method with minimal fields', () => {
     weaviate.graphql.get
       .things('Article', 'title url wordCount')
+      .do()
+      .then(function (result) {
+        expect(result.data.Get.Things.Article.length).toBeGreaterThan(0);
+      });
+  });
+
+  test('the typed graphql get method with optional fields', () => {
+    weaviate.graphql.get
+      .things('Article', 'title url wordCount')
+      .withExplore({concepts: ['news'], certainty: 0.1})
+      .withWhere({
+        operator: 'GreaterThanEqual',
+        path: ['wordCount'],
+        valueInt: 50,
+      })
       .do()
       .then(function (result) {
         expect(result.data.Get.Things.Article.length).toBeGreaterThan(0);
