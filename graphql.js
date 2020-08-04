02d77405385b33ed07cb3377af82afac6c21bce3
Etienne Dilocker <etienne@dilocker.de>
2020-08-04 10:06:27 +0200

    gh-1 add explore searchers
    and refactor existing code base to have one class per file

--- graphql.js
+++ graphql.js
@@ -1,3 +1,6 @@
+const where = require('./graphqlWhere.js');
+const explore = require('./graphqlExplore.js');
+
 const get = {
   things: function (className, queryString) {
     return new GraphQLBuilder('Things', className, queryString);
@@ -18,126 +21,34 @@ class GraphQLBuilder {
   }
 
   withWhere(whereObj) {
-    this.whereString = new GraphQLWhere(whereObj).toString();
+    this.whereString = new where.GraphQLWhere(whereObj).toString();
+    return this;
+  }
+
+  withExplore(exploreObj) {
+    this.exploreString = new explore.GraphQLExplore(exploreObj).toString();
     return this;
   }
 
   do() {
     let params = '';
 
-    if (this.whereString) {
-      params = `(where:${this.whereString})`;
+    if (this.whereString || this.exploreString) {
+      let args = [];
+
+      if (this.whereString) {
+        args = [...args, `where:${this.whereString}`];
+      }
+
+      if (this.exploreString) {
+        args = [...args, `explore:${this.exploreString}`];
+      }
+
+      params = `(${args.join(',')})`;
     }
 
     this.client.query(
       `{Get{${this.kind}{${this.className}${params}{${this.queryString}}}}}`,
     );
-  }
-}
-
-class GraphQLWhere {
-  constructor(whereObj) {
-    this.source = whereObj;
-  }
-
-  toString() {
-    this.parse();
-    this.validate();
-    if (this.operands) {
-      return (
-        `{` + `operator:${this.operator},` + `operands:[${this.operands}]` + `}`
-      );
-    } else {
-      // this is an on-value filter
-      return (
-        `{` +
-        `operator:${this.operator},` +
-        `${this.valueType}:${JSON.stringify(this.valueContent)},` +
-        `path:${JSON.stringify(this.path)}` +
-        `}`
-      );
-    }
-  }
-
-  validate() {
-    if (!this.operator) {
-      throw new Error('where filter: operator cannot be empty');
-    }
-
-    if (!this.operands) {
-      if (!this.valueType) {
-        throw new Error('where filter: value<Type> cannot be empty');
-      }
-
-      if (!this.path) {
-        throw new Error('where filter: path cannot be empty');
-      }
-    }
-  }
-
-  parse() {
-    for (let key in this.source) {
-      switch (key) {
-        case 'operator':
-          this.parseOperator(this.source[key]);
-          break;
-        case 'operands':
-          this.parseOperands(this.source[key]);
-          break;
-        case 'path':
-          this.parsePath(this.source[key]);
-          break;
-        default:
-          if (key.indexOf('value') != 0) {
-            throw new Error("where filter: unrecognized key '" + key + "'");
-          }
-          this.parseValue(key, this.source[key]);
-      }
-    }
-  }
-
-  parseOperator(op) {
-    if (typeof op !== 'string') {
-      throw new Error('where filter: operator must be a string');
-    }
-
-    this.operator = op;
-  }
-
-  parsePath(path) {
-    if (!Array.isArray(path)) {
-      throw new Error('where filter: path must be an array');
-    }
-
-    this.path = path;
-  }
-
-  parseValue(key, value) {
-    switch (key) {
-      case 'valueString':
-      case 'valueText':
-      case 'valueInt':
-      case 'valueNumber':
-      case 'valueDate':
-      case 'valueBoolean':
-      case 'valueGeoRange':
-        break;
-      default:
-        throw new Error("where filter: unrecognized value prop '" + key + "'");
-    }
-    this.valueType = key;
-    this.valueContent = value;
-  }
-
-  parseOperands(ops) {
-    if (!Array.isArray(ops)) {
-      throw new Error('where filter: operands must be an array');
-    }
-
-    this.operands = ops
-      .map(element => {
-        return new GraphQLWhere(element).toString();
-      })
-      .join(',');
   }
 }
--- graphql.test.js
+++ graphql.test.js
@@ -12,101 +12,203 @@ test('a simple query without params', () => {
   expect(mockClient.query).toHaveBeenCalledWith(expectedQuery);
 });
 
-test('a query with a valid where filter', () => {
-  const mockClient = {
-    query: jest.fn(),
-  };
+describe('where filters', () => {
+  test('a query with a valid where filter', () => {
+    const mockClient = {
+      query: jest.fn(),
+    };
 
-  const expectedQuery =
-    `{Get{Things{Person` +
-    `(where:{operator:Equal,valueString:"John Doe",path:["name"]})` +
-    `{name}}}}`;
+    const expectedQuery =
+      `{Get{Things{Person` +
+      `(where:{operator:Equal,valueString:"John Doe",path:["name"]})` +
+      `{name}}}}`;
 
-  gql.get
-    .things('Person', 'name')
-    .withClient(mockClient)
-    .withWhere({operator: 'Equal', valueString: 'John Doe', path: ['name']})
-    .do();
+    gql.get
+      .things('Person', 'name')
+      .withClient(mockClient)
+      .withWhere({operator: 'Equal', valueString: 'John Doe', path: ['name']})
+      .do();
 
-  expect(mockClient.query).toHaveBeenCalledWith(expectedQuery);
+    expect(mockClient.query).toHaveBeenCalledWith(expectedQuery);
+  });
+
+  test('a query with a valid nested where filter', () => {
+    const mockClient = {
+      query: jest.fn(),
+    };
+
+    const nestedWhere = {
+      operator: 'And',
+      operands: [
+        {valueString: 'foo', operator: 'Equal', path: ['foo']},
+        {valueString: 'bar', operator: 'NotEqual', path: ['bar']},
+      ],
+    };
+    const expectedQuery =
+      `{Get{Things{Person` +
+      `(where:{operator:And,operands:[` +
+      `{operator:Equal,valueString:"foo",path:["foo"]},` +
+      `{operator:NotEqual,valueString:"bar",path:["bar"]}` +
+      `]})` +
+      `{name}}}}`;
+
+    gql.get
+      .things('Person', 'name')
+      .withClient(mockClient)
+      .withWhere(nestedWhere)
+      .do();
+
+    expect(mockClient.query).toHaveBeenCalledWith(expectedQuery);
+  });
+
+  describe('queries with invalid nested where filters', () => {
+    const mockClient = {
+      query: jest.fn(),
+    };
+
+    const tests = [
+      {
+        title: 'an empty where',
+        where: {},
+        msg: 'where filter: operator cannot be empty',
+      },
+      {
+        title: 'missing value',
+        where: {operator: 'Equal'},
+        msg: 'where filter: value<Type> cannot be empty',
+      },
+      {
+        title: 'missing path',
+        where: {operator: 'Equal', valueString: 'foo'},
+        msg: 'where filter: path cannot be empty',
+      },
+      {
+        title: 'path is not an array',
+        where: {operator: 'Equal', valueString: 'foo', path: 'mypath'},
+        msg: 'where filter: path must be an array',
+      },
+      {
+        title: 'unknown value type',
+        where: {operator: 'Equal', valueWrong: 'foo'},
+        msg: "where filter: unrecognized value prop 'valueWrong'",
+      },
+      {
+        title: 'operands is not an array',
+        where: {operator: 'And', operands: {}},
+        msg: 'where filter: operands must be an array',
+      },
+    ];
+
+    tests.forEach(t => {
+      test(t.title, () => {
+        expect(() => {
+          gql.get
+            .things('Person', 'name')
+            .withClient(mockClient)
+            .withWhere(t.where)
+            .do();
+        }).toThrow(t.msg);
+      });
+    });
+  });
 });
 
-test('a query with a valid nested where filter', () => {
-  const mockClient = {
-    query: jest.fn(),
-  };
+describe('explore searchers', () => {
+  test('a query with a valid explore', () => {
+    const mockClient = {
+      query: jest.fn(),
+    };
 
-  const nestedWhere = {
-    operator: 'And',
-    operands: [
-      {valueString: 'foo', operator: 'Equal', path: ['foo']},
-      {valueString: 'bar', operator: 'NotEqual', path: ['bar']},
-    ],
-  };
-  const expectedQuery =
-    `{Get{Things{Person` +
-    `(where:{operator:And,operands:[` +
-    `{operator:Equal,valueString:"foo",path:["foo"]},` +
-    `{operator:NotEqual,valueString:"bar",path:["bar"]}` +
-    `]})` +
-    `{name}}}}`;
+    const expectedQuery =
+      `{Get{Things{Person` + `(explore:{concepts:["foo","bar"]})` + `{name}}}}`;
 
-  gql.get
-    .things('Person', 'name')
-    .withClient(mockClient)
-    .withWhere(nestedWhere)
-    .do();
+    gql.get
+      .things('Person', 'name')
+      .withClient(mockClient)
+      .withExplore({concepts: ['foo', 'bar']})
+      .do();
 
-  expect(mockClient.query).toHaveBeenCalledWith(expectedQuery);
-});
+    expect(mockClient.query).toHaveBeenCalledWith(expectedQuery);
+  });
 
-describe('queries with invalid nested where filters', () => {
-  const mockClient = {
-    query: jest.fn(),
-  };
+  test('with optional parameters', () => {
+    const mockClient = {
+      query: jest.fn(),
+    };
 
-  const tests = [
-    {
-      title: 'an empty where',
-      where: {},
-      msg: 'where filter: operator cannot be empty',
-    },
-    {
-      title: 'missing value',
-      where: {operator: 'Equal'},
-      msg: 'where filter: value<Type> cannot be empty',
-    },
-    {
-      title: 'missing path',
-      where: {operator: 'Equal', valueString: 'foo'},
-      msg: 'where filter: path cannot be empty',
-    },
-    {
-      title: 'path is not an array',
-      where: {operator: 'Equal', valueString: 'foo', path: 'mypath'},
-      msg: 'where filter: path must be an array',
-    },
-    {
-      title: 'unknown value type',
-      where: {operator: 'Equal', valueWrong: 'foo'},
-      msg: "where filter: unrecognized value prop 'valueWrong'",
-    },
-    {
-      title: 'operands is not an array',
-      where: {operator: 'And', operands: {}},
-      msg: 'where filter: operands must be an array',
-    },
-  ];
+    const expectedQuery =
+      `{Get{Things{Person` +
+      `(explore:{concepts:["foo","bar"],certainty:0.7,moveTo:{concepts:["foo"],force:0.7},moveAwayFrom:{concepts:["bar"],force:0.5}})` +
+      `{name}}}}`;
 
-  tests.forEach(t => {
-    test(t.title, () => {
-      expect(() => {
-        gql.get
-          .things('Person', 'name')
-          .withClient(mockClient)
-          .withWhere(t.where)
-          .do();
-      }).toThrow(t.msg);
+    gql.get
+      .things('Person', 'name')
+      .withClient(mockClient)
+      .withExplore({
+        concepts: ['foo', 'bar'],
+        certainty: 0.7,
+        moveTo: {concepts: ['foo'], force: 0.7},
+        moveAwayFrom: {concepts: ['bar'], force: 0.5},
+      })
+      .do();
+
+    expect(mockClient.query).toHaveBeenCalledWith(expectedQuery);
+  });
+
+  describe('queries with invalid explore searchers', () => {
+    const mockClient = {
+      query: jest.fn(),
+    };
+
+    const tests = [
+      {
+        title: 'an empty explore',
+        explore: {},
+        msg: 'explore filter: concepts cannot be empty',
+      },
+      {
+        title: 'concepts of wrong type',
+        explore: {concepts: {}},
+        msg: 'explore filter: concepts must be an array',
+      },
+      {
+        title: 'certainty of wrong type',
+        explore: {concepts: ['foo'], certainty: 'foo'},
+        msg: 'explore filter: certainty must be a number',
+      },
+      {
+        title: 'moveTo without concepts',
+        explore: {concepts: ['foo'], moveTo: {}},
+        msg: 'explore filter: moveTo.concepts must be an array',
+      },
+      {
+        title: 'moveTo without concepts',
+        explore: {concepts: ['foo'], moveTo: {concepts: ['foo']}},
+        msg: "explore filter: moveTo must have fields 'concepts' and 'force'",
+      },
+      {
+        title: 'moveAwayFrom without concepts',
+        explore: {concepts: ['foo'], moveAwayFrom: {}},
+        msg: 'explore filter: moveAwayFrom.concepts must be an array',
+      },
+      {
+        title: 'moveAwayFrom without concepts',
+        explore: {concepts: ['foo'], moveAwayFrom: {concepts: ['foo']}},
+        msg:
+          "explore filter: moveAwayFrom must have fields 'concepts' and 'force'",
+      },
+    ];
+
+    tests.forEach(t => {
+      test(t.title, () => {
+        expect(() => {
+          gql.get
+            .things('Person', 'name')
+            .withClient(mockClient)
+            .withExplore(t.explore)
+            .do();
+        }).toThrow(t.msg);
+      });
     });
   });
 });
