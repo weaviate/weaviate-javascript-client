f92d43098532482fdb45663eed83df1ec4cf9790
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-10-07 15:40:44 +0100

    WVT-363: adds support for /nodes API

---
+++ misc/consts.js
@@ -0,0 +1,11 @@
+export const NodeStatus = {
+  HEALTHY: "HEALTHY",
+  UNHEALTHY: "UNHEALTHY",
+  UNAVAILABLE: "UNAVAILABLE",
+};
+
+const miscConsts = {
+  NodeStatus,
+};
+
+export default miscConsts;
